e90ed5694dafe7f3170b4be3ab4439b7e840ee2d
hamsedirie6 <hamse_03@hotmail.se>
2025-12-03 16:12:21 +0100

    skapade datalager med modeller, DbContext och första EF Core-migrationen, ändrat så att databasen inte comittas

---
+++ backend/SocialNetwork.Entity/Models/DirectMessage.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Entity.Models
+{
+    public class DirectMessage
+    {
+        public int Id { get; set; }
+        public int FromUserId { get; set; }
+        public int ToUserId { get; set; }
+        public string Message { get; set; }
+        public DateTime SentAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ backend/SocialNetwork.Entity/Models/Follow.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Entity.Models
+{
+    public class Follow
+    {
+        public int Id { get; set; }
+        public int FollowerId { get; set; }
+        public int FollowingId { get; set; }
+    }
+}
