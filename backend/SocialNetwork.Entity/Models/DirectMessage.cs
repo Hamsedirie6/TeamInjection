c8c4ca290addf9e7406e62ced5aff5e849495434
hamsedirie6 <hamse_03@hotmail.se>
2025-12-05 17:22:37 +0100

    Ändrat namn på egenskaper

--- backend/SocialNetwork.Entity/Models/DirectMessage.cs
+++ backend/SocialNetwork.Entity/Models/DirectMessage.cs
@@ -8,9 +8,10 @@ namespace SocialNetwork.Entity.Models
     public class DirectMessage
     {
         public int Id { get; set; }
-        public int FromUserId { get; set; }
-        public int ToUserId { get; set; }
+        public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
         public string Message { get; set; }
         public DateTime SentAt { get; set; } = DateTime.UtcNow;
     }
+
 }
