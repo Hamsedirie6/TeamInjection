428661ba9520f5da5547d2ac30b1e43fcfbb2272
hamsedirie6 <hamse_03@hotmail.se>
2025-12-05 17:24:34 +0100

    Logiken för att testerna ska lyckas

--- backend/Socialnetwork.Repository/Services/PostService.cs
+++ backend/Socialnetwork.Repository/Services/PostService.cs
@@ -17,9 +17,21 @@ public class PostService
         if (string.IsNullOrWhiteSpace(post.Message))
             return (false, "Message is required");
 
+        post.CreatedAt = DateTime.UtcNow;
+
         _context.Posts.Add(post);
         _context.SaveChanges();
 
         return (true, "");
+    }
+
+    public Post? GetById(int id)
+    {
+        return _context.Posts.FirstOrDefault(p => p.Id == id);
+    }
+
+    public IEnumerable<Post> GetAll()
+    {
+        return _context.Posts.ToList();
     }
 }
